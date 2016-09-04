96890f4a10f29be2afbce0a4d4c52ed6f65f1996
Pavlos Vinieratos <pvinis@gmail.com>
2016-09-04 13:34:03 +0200

    Initial Commit

---
+++ StreamAndKeep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StreamAndKeep
+//
+//  Created by Pavlos Vinieratos on 04/09/16.
+//  Copyright © 2016 Pavlos Vinieratos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
